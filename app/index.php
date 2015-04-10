b2ec18fb959b506f012d61c65e5ffcf5c8eee22b
kien tran <kien@tutoruniverse.net>
2015-04-10 10:59:07 +0700

    Change the way get headers

--- app/index.php
+++ app/index.php
@@ -1,15 +1,7 @@
 <?php
 
 function parseRequestHeaders() {
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) <> 'HTTP_') {
-            continue;
-        }
-        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-        $headers[$header] = $value;
-    }
-    return $headers;
+    return (array)apache_request_headers();
 }
 $headers = parseRequestHeaders();
 $authenKey = isset($headers["Authorization"])?$headers["Authorization"]:"";
