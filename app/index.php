327c0efb0b85005165612ce061a6b0c8ea56b55c
kien tran <kien@tutoruniverse.net>
2015-04-21 15:59:46 +0700

    Get headers if server not running apache

--- app/index.php
+++ app/index.php
@@ -1,4 +1,19 @@
 <?php
+if (!function_exists('apache_request_headers')) 
+{ 
+    function apache_request_headers() 
+    { 
+       $headers = array(); 
+       foreach ($_SERVER as $name => $value) 
+       { 
+           if (substr($name, 0, 5) == 'HTTP_') 
+           { 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
+} 
 
 function parseRequestHeaders() {
     return (array)apache_request_headers();
